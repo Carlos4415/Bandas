10261d7323773273177af552128822037ae4cce2
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-02 11:29:05 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Este projeto consiste em criar uma lista vazia para armazenar nomes de bandas e, em seguida, 
+// adicionar à lista as bandas favoritas do usuário. O projeto permite praticar o uso de coleções 
+// (List) em C#, como criar uma lista, adicionar elementos dinamicamente e manipular seus dados.
+List<string> bandas = new List<string>();
+
+bandas.Add("The Beatles");
+bandas.Add("Pink Floyd");
+bandas.Add("U2");
+bandas.Add("Ira!");
+
+for (int i = 0; i < bandas.Count; i++)
+{
+    Console.WriteLine(bandas[i]);
+}
